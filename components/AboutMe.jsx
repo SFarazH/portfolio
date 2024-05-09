c169af0b3a1826b4af958beae89fafa9245ebb00
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-05-10 00:43:43 +0530

    add skills

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -2,22 +2,31 @@
 import React from "react";
 import Image from "next/image";
 import img from "../public/me.jpg";
+import Skills from "./Skills";
 
 export default function AboutMe() {
   return (
     <>
-      <div class="container px-4 md:py-10 md:px-0 flex border-b">
-        <div class="md:w-2/3 mt-8 pr-8">
-          <h3 class="text-3xl lg:text-5xl font-bold">Faraz Hasan</h3>
-          <h3 class="text-xl lg:text-2xl my-2 text-green-500">
+      <div className="container px-4 md:py-10 md:px-0 flex border-b">
+        <div className="md:w-2/3 mt-8 pr-8">
+          <h3 className="text-3xl lg:text-5xl font-bold">Faraz Hasan</h3>
+          <h3 className="text-xl lg:text-2xl my-2 text-green-500">
             Student, Full Stack Developer;
           </h3>
-          <p className="text-gray-400 mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint aut in, ut, laborum nemo nostrum quidem nobis veniam numquam amet accusantium modi culpa ad asperiores. Cumque minima quidem expedita magnam? Quisquam numquam eaque fugit, vitae molestiae reiciendis in. Molestiae minima id quo alias perferendis eveniet, provident doloribus reiciendis fugiat amet!</p>
+          <p className="text-gray-400 mt-5">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint aut
+            in, ut, laborum nemo nostrum quidem nobis veniam numquam amet
+            accusantium modi culpa ad asperiores. Cumque minima quidem expedita
+            magnam? Quisquam numquam eaque fugit, vitae molestiae reiciendis in.
+            Molestiae minima id quo alias perferendis eveniet, provident
+            doloribus reiciendis fugiat amet!
+          </p>
         </div>
-        <div class="md:w-1/3">
-            <Image src={img} className="rounded-lg"></Image>
+        <div className="md:w-1/3">
+          <Image src={img} className="rounded-lg"></Image>
         </div>
       </div>
+      <Skills />
     </>
   );
 }
---
+++ components/Skills.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import react from "../public/icons/React.svg";
+import tailwind from "../public/icons/Tailwind.svg";
+import bs from "../public/icons/Bootstrap.svg";
+import js from "../public/icons/JavaScript.svg";
+import Next from "../public/icons/nextjs.svg";
+import ex from "../public/icons/Express.svg";
+import mongo from "../public/icons/MongoDB.svg";
+import aws from "../public/icons/AWS.svg";
+import dj from "../public/icons/Django.svg";
+import node from "../public/icons/Node.js.svg";
+import sql from "../public/icons/MySQL.svg";
+import py from "../public/icons/Python.svg";
+import j from "../public/icons/Jupyter.svg";
+import pyt from "../public/icons/PyTorch.svg";
+import r from "../public/icons/R.svg";
+import Image from "next/image";
+
+export default function Skills() {
+  return (
+    <>
+      <div className="container my-4">
+        <h2 className="text-3xl font-bold">Skills</h2>
+        <div className="my-4">
+
+          <p className="text-xl py-1 ">Frontend</p>
+          <div className="flex gap-8 py-1">
+            <Image src={js} width={60} className="rounded-lg" />
+            <Image src={react} width={60} />
+            <Image src={Next} width={60} className="rounded-lg" />
+            <Image src={tailwind} width={60} />
+            <Image src={bs} width={60} />
+          </div>
+        </div>
+        <div className="mt-3 mb-4">
+
+          <p className="text-xl py-1 ">Backend / DevOps</p>
+          <div className="flex gap-8 py-1">
+            <Image src={node} width={60} />
+            <Image src={ex} width={60} style={{ color: "white" }} />
+            <Image src={dj} width={60} />
+            <Image src={aws} width={60} />
+            <Image src={mongo} width={60} />
+            <Image src={sql} width={65} />
+          </div>
+        </div>
+        <div className="mt-2 mb-4">
+          <p className="text-xl py-1 ">Machine Learning / Data Science</p>
+          <div className="flex gap-8 py-1">
+            <Image src={py} width={60} />
+            <Image src={r} width={60} />
+            <Image src={j} width={60} />
+            <Image src={pyt} width={60} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
