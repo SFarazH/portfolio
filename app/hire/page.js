bdaba47df602ba46fd7eb47282d501cf33195441
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-07-12 12:00:00 +0530

    update hire-me page

--- app/hire/page.js
+++ app/hire/page.js
@@ -29,7 +29,7 @@ export default function Home() {
         <>
           <main className={`flex fira flex-col justify-between px-4 md:px-0`}>
             <h2 className="text-3xl font-semibold text-center mt-8 mb-8">
-              Hire me !
+              Hire me!
             </h2>
             <p className="font-light text-lg">
               Hello, my name is Faraz Hasan. <br /> I am a 2024{" "}
@@ -39,26 +39,30 @@ export default function Home() {
               and a CGPA of{" "}
               <span className="bg-red-500 rounded-lg px-1.5">9.05</span>. <br />
               <br /> I have worked as a{" "}
-              <span className="bg-green-600 rounded-lg px-1.5">
+              <span className="bg-amber-400 text-black rounded-lg px-1.5">
                 Full Stack Intern
               </span>{" "}
               at OneBigBit Technologies Pvt. Ltd. During my internship, I
               focussed on developing applications using{" "}
-              <span className="bg-green-600 rounded-lg px-1.5">MERN</span> stack
-              technology and{" "}
-              <span className="bg-green-600  rounded-lg px-1.5">Firebase</span>.
-              I have worked on multiple live projects, contributing to both{" "}
-              <span className="bg-green-600 rounded-lg px-1.5">
+              <span className="bg-amber-400 text-black rounded-lg px-1.5">
+                MERN
+              </span>{" "}
+              stack technology and{" "}
+              <span className="bg-amber-400 text-black rounded-lg px-1.5">
+                Firebase
+              </span>
+              . I have worked on multiple live projects, contributing to both{" "}
+              <span className="bg-amber-400 text-black rounded-lg px-1.5">
                 frontend and backend
               </span>{" "}
               development, including using Electron.js to deliver web apps.{" "}
               <br /> Along with this, I have also delivered independant projects
               including{" "}
-              <span className="bg-green-600 rounded-lg px-1.5">
+              <span className="bg-amber-400 text-black rounded-lg px-1.5">
                 Tourism Website
               </span>{" "}
               - enabled with RBAC; and{" "}
-              <span className="bg-green-600 rounded-lg px-1.5">
+              <span className="bg-amber-400 text-black rounded-lg px-1.5">
                 Company Finances Manager
               </span>{" "}
               - to help the company view all their finances at a single place,
@@ -66,18 +70,18 @@ export default function Home() {
               for data visualization. <br /> <br />
               During my college semeters, I have also worked on two semester
               projects, focussing on{" "}
-              <span className="bg-blue-700 rounded-lg px-1.5">
+              <span className="bg-red-500 rounded-lg px-1.5">
                 Image matching
               </span>{" "}
               using LoFTR and{" "}
-              <span className="bg-blue-700 rounded-lg px-1.5">
+              <span className="bg-red-500 rounded-lg px-1.5">
                 Skin Cancer Detection
               </span>{" "}
               using ResNet . Both of these research papers have been published
               at different conferrences - namely{" "}
-              <span className="bg-blue-700 rounded-lg px-1.5">DAPCOM 2024</span>{" "}
+              <span className="bg-red-500 rounded-lg px-1.5">DAPCOM 2024</span>{" "}
               and{" "}
-              <span className="bg-blue-700 rounded-lg px-1.5">AITC 2024</span>.
+              <span className="bg-red-500 rounded-lg px-1.5">AITC 2024</span>.
               <br />
               <br /> Regarding my technical skills, I am proficient in MERN
               stack, having architected multiple applications with features such
@@ -97,7 +101,8 @@ export default function Home() {
               experience in using{" "}
               <span className="bg-amber-400 text-black rounded-lg px-1.5">
                 Python
-              </span>{" "}and {" "}
+              </span>{" "}
+              and{" "}
               <span className="bg-amber-400 text-black rounded-lg px-1.5">
                 SQL
               </span>{" "}
@@ -113,10 +118,18 @@ export default function Home() {
               model which uses previous stock data across different time periods
               and predicts stocks likely to perform well.
               <br /> I also have experience across different products of{" "}
-              <span className="bg-red-500 rounded-lg px-1.5">AWS</span> - such
-              as <span className="bg-red-500 rounded-lg px-1.5">EC2</span> and{" "}
-              <span className="bg-red-500 rounded-lg px-1.5">Lambda</span>,
-              which I have used to host live projects. <br />
+              <span className="bg-amber-400 text-black rounded-lg px-1.5">
+                AWS
+              </span>{" "}
+              - such as{" "}
+              <span className="bg-amber-400 text-black rounded-lg px-1.5">
+                EC2
+              </span>{" "}
+              and{" "}
+              <span className="bg-amber-400 text-black rounded-lg px-1.5">
+                Lambda
+              </span>
+              , which I have used to host live projects. <br />
               <br />
               Additionally, I have completed multiple certification courses
               across various fields such as:
@@ -129,11 +142,11 @@ export default function Home() {
               <br />
               Regarding my extra-cirriculars, I was part of a team which was
               shortlisted for{" "}
-              <span className="bg-teal-700 rounded-lg px-1.5">
+              <span className="bg-red-500 rounded-lg px-1.5">
                 Smart India Hackathon
               </span>{" "}
               finals 2022 at Mangalore. I have also served as{" "}
-              <span className="bg-teal-700 rounded-lg px-1.5">President</span> of
+              <span className="bg-red-500 rounded-lg px-1.5">President</span> of
               the CSE student body. This has instilled me with skills to manage
               people and deadlines, along with collaboration and team work!{" "}
               <br />
