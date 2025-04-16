ccde8e0bdc06da056d273dd54f102a86cb6cd5b8
SFarazH <75844866+SFarazH@users.noreply.github.com>
2025-04-16 17:58:40 +0530

    add skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -14,9 +14,16 @@ import py from "../public/icons/Python.svg";
 import j from "../public/icons/Jupyter.svg";
 import pyt from "../public/icons/PyTorch.svg";
 import r from "../public/icons/R.svg";
+import java from "../public/icons/Java.svg";
+import spring from "../public/icons/Spring.svg";
 import Image from "next/image";
 
 export default function Skills() {
+  const options = {
+    alt: "logo",
+    width: 60,
+    className: "rounded-lg",
+  };
   return (
     <>
       <div className="px-4 md:px-0 border-b py-12">
@@ -25,31 +32,40 @@ export default function Skills() {
         <div className="mt-4 mb-12">
           <p className="text-2xl py-1 text-[#FFF8DC] ">Frontend</p>
           <div className="flex flex-wrap gap-8 py-1">
-            <Image alt='logo' src={js} width={60} className="rounded-lg" />
-            <Image alt='logo' src={react} width={60} />
-            <Image alt='logo' src={Next} width={60} className="rounded-lg" />
-            <Image alt='logo' src={tailwind} width={60} />
-            <Image alt='logo' src={bs} width={60} />
+            <Image title="Javascript" src={js} {...options} />
+            <Image title="React" src={react} {...options} />
+            <Image title="Next" src={Next} {...options} />
+            <Image title="Tailwind" src={tailwind} {...options} />
+            <Image title="Bootstrap" src={bs} {...options} />
           </div>
         </div>
         <div className="my-12">
           <p className="text-2xl py-1 text-[#FFF8DC]">Backend / Database</p>
           <div className="flex flex-wrap gap-8 py-1">
-            <Image alt='logo' src={node} width={60} />
-            <Image alt='logo' src={ex} width={60} style={{ color: "white" }} />
-            <Image alt='logo' src={dj} width={60} />
-            <Image alt='logo' src={aws} width={60} />
-            <Image alt='logo' src={mongo} width={60} />
-            <Image alt='logo' src={sql} width={65} />
+            <Image title="Node" src={node} {...options} />
+            <Image
+              title="ExpressJS"
+              src={ex}
+              style={{ color: "white" }}
+              {...options}
+            />
+            <Image title="Java" src={java} {...options} />
+            <Image title="Spring" src={spring} {...options} />
+            <Image title="Django" src={dj} {...options} />
+            <Image title="AWS Console" src={aws} {...options} />
+            <Image title="MongoDB" src={mongo} {...options} />
+            <Image title="SQL" src={sql} {...options} width={65} />
           </div>
         </div>
         <div className="mt-12">
-          <p className="text-2xl py-1 text-[#FFF8DC]">Machine Learning / Data Science</p>
+          <p className="text-2xl py-1 text-[#FFF8DC]">
+            Machine Learning / Data Science
+          </p>
           <div className="flex flex-wrap gap-8 py-1">
-            <Image alt='logo' src={py} width={60} />
-            <Image alt='logo' src={r} width={60} />
-            <Image alt='logo' src={j} width={60} />
-            <Image alt='logo' src={pyt} width={60} />
+            <Image title="Python" src={py} {...options} />
+            <Image title="R" src={r} {...options} />
+            <Image title="Jupyter Notebook" src={j} {...options} />
+            <Image title="PyTorch" src={pyt} {...options} />
           </div>
         </div>
       </div>
